ec13edd35731fba747697086776fb9c3cacf3827
Hurley <hurleywflow@gmail.com>
2024-07-25 22:41:34 +1000

    feat: :art: const defaultLocale = request.headers.get("x-your-custom-locale") as "vi" | "en" ?? "en";

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
 import createMiddleware from "next-intl/middleware";
+import type { NextRequest } from "next/server";
+import { locales } from "./config";
 
-export default createMiddleware({
-	// A list of all locales that are supported
-	locales: ["en", "vi"],
+export default async function middleware(request: NextRequest) {
+	// Step 1: Use the incoming request (example)
+	const defaultLocale = request.headers.get("x-your-custom-locale") as "vi" | "en" ?? "en";
 
-	// Used when no locale matches
-	defaultLocale: "en",
-});
+	// Step 2: Create and call the next-intl middleware (example)
+	const handleI18nRouting = createMiddleware({
+		locales,
+		defaultLocale,
+	});
+	const response = handleI18nRouting(request);
+
+	// Step 3: Alter the response (example)
+	response.headers.set("x-your-custom-locale", defaultLocale);
+
+	return response;
+}
 
 export const config = {
 	// Match only internationalized pathnames
